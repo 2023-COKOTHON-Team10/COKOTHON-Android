ed44f7f6b2b93e255dc9505568c6926d7825f3c2
Eonji-sw <ejkim0625@gmail.com>
2023-12-26 22:44:53 +0900

    [FEAT] : 퀴즈 답변 페이지 구현

---
+++ app/src/main/java/com/example/cokothon/presentation/QuestionAnswerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cokothon.presentation
+
+import com.example.cokothon.R
+import com.example.cokothon.core.base.BindingActivity
+import com.example.cokothon.databinding.ActivityQuestionAnswerBinding
+
+class QuestionAnswerActivity : BindingActivity<ActivityQuestionAnswerBinding>(R.layout.activity_question_answer) {
+    override fun initView() {
+        // 로직 작성
+
+    }
+}
