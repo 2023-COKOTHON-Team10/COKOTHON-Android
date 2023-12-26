20a9a735e57dce4fe51687b89122066871b2c25f
so1h0lyu <ssssssun56@sookmyung.ac.kr>
2023-12-26 19:00:39 +0900

    quiz number 액티비티 ui

---
+++ app/src/main/java/com/example/cokothon/presentation/QuizNumberActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cokothon.presentation
+
+import com.example.cokothon.R
+import com.example.cokothon.core.base.BindingActivity
+import com.example.cokothon.databinding.ActivityMainBinding
+
+class QuizNumberActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_quiz_number) {
+    override fun initView() {
+        // 로직 작성
+    }
+}
