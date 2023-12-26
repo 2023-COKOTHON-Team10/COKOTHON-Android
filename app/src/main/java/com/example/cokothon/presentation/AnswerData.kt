604de77fcd074e54e10114b10ddfa753c7b94f1f
so1h0lyu <ssssssun56@sookmyung.ac.kr>
2023-12-26 22:08:45 +0900

    data class 생성

---
+++ app/src/main/java/com/example/cokothon/presentation/AnswerData.kt
@@ -0,0 +1,11 @@
+package com.example.cokothon.presentation
+
+data class AnswerData(
+    val user_id: Long,
+    val ans1: Long,
+    val ans2: Long,
+    val ans3: Long,
+    val ans4: Long,
+    val ans5: Long,
+    val unique_number: String
+)
